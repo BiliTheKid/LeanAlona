2897e65fc0cb68ad9ec5d4aaca61e8c1275aacad
mickey hovel <mickeyhovel@gmail.com>
2024-09-18 11:19:11 +0300

    fixing docker run

--- docker_run.sh
+++ docker_run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Set default Docker image name if not provided
 IMAGE_NAME=${1:-alona-bot}
-#docker run "$(grep -v '^#' .env | grep -v '^$' | sed 's/^/--env /')" -p 8000:8000 -d "$IMAGE_NAME"
-docker run --env-file .env -p 8000:8000 -d "$IMAGE_NAME"
-
+DATABASE_URL=$(grep DATABASE_URL .env |grep -v '^#' | cut -d '=' -f2)
+docker run --env-file .env --env DATABASE_URL="$(grep DATABASE_URL .env |grep -v '^#' | cut -d '=' -f2)"  -p 8000:8000 -d "$IMAGE_NAME"
+#docker run --env-file .env --env DATABASE_URL="$DATABASE_URL"  -p 8000:8000 -d "$IMAGE_NAME"
+#docker run --env-file .env -p 8000:8000 -d "$IMAGE_NAME"
